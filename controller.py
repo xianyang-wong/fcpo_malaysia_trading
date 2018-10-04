12b7f6192e8937e943b2045f7ab90d9fe33a38a6
brucelee919@gmail.com <qingtao@Qings-iMac.local>
2018-10-04 21:11:36 +0800

    seperate the logic

--- controller.py
+++ controller.py
@@ -10,14 +10,9 @@ import pandas as pd
 import parse as parsed
 import os
 
-directory = '/Users/qingtao/OneDrive - National University of Singapore/MTech/01 KE5207/computational intelligence ii/Lecture Notes/Day 3 (Sam GU Zhan)/GA-Fuzzy CA - Algorithmic Trading System/'
-data = pd.read_excel(os.path.join(directory,'data/FCPO_6_years_NUS.xlsx'))
-data.columns = data.columns.str.strip()
-parsed = parsed.parse_futures_data(data, ['Open','High','Low','Close','Volume'])
 
-parsed['flag'] = parsed['Date'].diff()
-parsed.loc[parsed.Date != parsed.Date.shift(-1), 'flag'] = 2 # 2=closing time
-parsed.loc[parsed['flag']>2,'flag']=1 # 1=opening time
+directory = '/Users/qingtao/OneDrive - National University of Singapore/MTech/01 KE5207/computational intelligence ii/CA/fcpo_malaysia_trading/'
+parsed = pd.read_excel(os.path.join(directory,'data/FCPO_6_years_NUS_Parsed.xlsx'))
 
 print('--------------')
 print(len(parsed))
@@ -38,8 +33,8 @@ for i in range (0, 69):
     print('Group'+ str(i+1) + ' Size: ' + str(x))
     for j in range (0,4):
         y1 += subGroupSize
-        y0 = y1 + subGroupSize
-        y2 = y0 + subGroupSize
+        yTmp = y1 + subGroupSize
+        y2 = yTmp + subGroupSize
         y3 = y2
         y4 = y3 + subGroupSize
         print('Sub Group Index 1: '+ str(y1))
@@ -51,10 +46,4 @@ for i in range (0, 69):
     y1=y4
     # call fitness function
 #    FF = FitnessFunction()
-        
-import datetime
-now = datetime.datetime.now()
-out_path = directory
-writer = pd.ExcelWriter(os.path.join(directory,'data/FCPO_6_years_NUS '+ now.strftime("%Y-%m-%d %H-%M-%S")+'.xlsx'))
-parsed.to_excel(writer)
-writer.save()    
+
